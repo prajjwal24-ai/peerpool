3a86c429ee54e916eea0990d5d0c10167c04f957
prajjwal24-indevs <prajjwal.katiyar@theindevs.com>
2026-07-14 15:16:20 +0530

    middleware

--- src/middleware/authMiddleware.js
+++ src/middleware/authMiddleware.js
@@ -1,7 +1,7 @@
 import jwt from 'jsonwebtoken';
 import { User } from '../models/userModel.js';
 
-export const protect = async (req,res, next){
+export const protect = async (req,res, next)=>{
     let token;
 
     if(req.headers.authorization && req.headers.authorization.startWith('Bearer')){
