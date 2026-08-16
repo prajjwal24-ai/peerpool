f3f8f18af7241061836c684c6f8e38c17f921a17
prajjwal24-indevs <prajjwal.katiyar@theindevs.com>
2026-08-16 19:53:26 +0530

    Configure dynamic backend URL with VITE_BACKEND_URL

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 const API = axios.create({
-    baseURL : 'http://localhost:5000/api',
+    baseURL: `${import.meta.env.VITE_BACKEND_URL || "http://localhost:5000"}/api`,
     headers : {
         'Content-Type': 'application/json'
     }
